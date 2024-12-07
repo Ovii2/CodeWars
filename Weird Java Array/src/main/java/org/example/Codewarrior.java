236820613b8ab21856a53dce15554628a7272083
Ovidijus <ovidijus.e@gmail.com>
2024-12-07 07:29:35 +0200

    Weird java array

---
+++ Weird Java Array/src/main/java/org/example/Codewarrior.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.List;
+
+class Codewarrior {
+    public static List[] arrayOfLists = new List[]{
+            List.of(1, 2, 3),
+            List.of(4, 5, 6),
+            List.of(7, 8, 9),
+    };
+}
