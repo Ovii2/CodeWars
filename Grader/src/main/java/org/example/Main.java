063a880c4d5b95655bb68d8aa8696ae585cae9cd
Ovidijus <ovidijus.e@gmail.com>
2024-12-11 08:49:35 +0200

    Grader

---
+++ Grader/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Grader.grader(0.7));
+        System.out.println(Grader.grader(0.9));
+        System.out.println(Grader.grader(0.6));
+        System.out.println(Grader.grader(1.1));
+        System.out.println(Grader.grader(1.01));
+
+    }
+}
