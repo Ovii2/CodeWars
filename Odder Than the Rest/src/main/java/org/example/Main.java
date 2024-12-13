436b14b5f55e6e9c17e4c9bfdb1a3afa0e62860e
Ovidijus <ovidijus.e@gmail.com>
2024-12-13 18:30:42 +0200

    Odder Than the Rest

---
+++ Odder Than the Rest/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Solution.oddOne(new int[]{2, 4, 6, 7, 10}));
+        System.out.println(Solution.oddOne(new int[]{2, 16, 98, 10, 13, 78}));
+        System.out.println(Solution.oddOne(new int[]{4, -8, 98, -12, -7, 90, 100}));
+        System.out.println(Solution.oddOne(new int[]{2, 4, 6, 8}));
+    }
+}
---
+++ Odder Than the Rest/src/main/java/org/example/Solution.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Solution {
+    public static int oddOne(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 != 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
