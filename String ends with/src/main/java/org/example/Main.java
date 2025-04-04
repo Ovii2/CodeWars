8ebbbfd14a5882eb4fe37d7773b5d155bd285087
Ovidijus <ovidijus.e@gmail.com>
2025-04-04 08:11:33 +0300

    String ends with

---
+++ String ends with/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Kata.solution("abc", "bc"));
+        System.out.println(Kata.solution("abc", "d"));
+        System.out.println(Kata.solution("samurai", "ai"));
+    }
+}
