0722ab591fb71474d8a57ad7dd70af5fd42aeba1
Ovidijus <ovidijus.e@gmail.com>
2025-04-07 16:41:08 +0300

    Simple beads count

---
+++ Simple beads count/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(BeadsCounter.countRedBeads(0));
+        System.out.println(BeadsCounter.countRedBeads(1));
+        System.out.println(BeadsCounter.countRedBeads(3));
+        System.out.println(BeadsCounter.countRedBeads(5));
+    }
+}
