af62ea87febe52d131919f91e63c6b87de13a106
Ovidijus <ovidijus.e@gmail.com>
2025-04-11 09:10:11 +0300

    Regexp Basics - is it a vowel?

---
+++ Regexp Basics - is it a vowel/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.isVowel(""));
+        System.out.println(StringUtils.isVowel("a"));
+        System.out.println(StringUtils.isVowel("E"));
+        System.out.println(StringUtils.isVowel("ou"));
+        System.out.println(StringUtils.isVowel("z"));
+        System.out.println(StringUtils.isVowel("lol"));
+    }
+}
---
+++ Regexp Basics - is it a vowel/src/main/java/org/example/StringUtils.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class StringUtils {
+    public static boolean isVowel(String s) {
+        return s.matches("[aeiouAEIOU]");
+    }
+}
