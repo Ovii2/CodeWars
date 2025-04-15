6c9d5b6bbcfcd8b3adf93ac5c7f8c62817b57b8e
Ovidijus <ovidijus.e@gmail.com>
2025-04-15 18:07:47 +0300

    Perimeter sequence

---
+++ Perimeter sequence/src/main/java/org/example/Kata.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Kata {
+    public static int perimeterSequence(int a, int n) {
+        return a * 4 * n;
+    }
+}
---
+++ Perimeter sequence/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Kata.perimeterSequence(1, 3));
+    }
+}
