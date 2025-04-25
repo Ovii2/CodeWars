d0a77bf212b8bde30fc12b2f37366568206256bb
Ovidijus <ovidijus.e@gmail.com>
2025-04-25 17:21:37 +0300

    Sum of Two Integers

---
+++ Sum of Two Integers/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Solution.add(5, 19));
+        System.out.println(Solution.add(-27, 18));
+        System.out.println(Solution.add(-14, -16));
+    }
+}
---
+++ Sum of Two Integers/src/main/java/org/example/Solution.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Solution {
+    public static int add(int x, int y) {
+        return x \u002b y;
+    }
+}
