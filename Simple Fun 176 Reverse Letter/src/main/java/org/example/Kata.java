93efc4b7b1570261a5e66da680e2fff2334830ad
Ovidijus <ovidijus.e@gmail.com>
2025-05-08 19:06:53 +0300

    Simple Fun #176: Reverse Letter

---
+++ Simple Fun 176 Reverse Letter/src/main/java/org/example/Kata.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Kata {
+    public static String reverseLetter(final String str) {
+        return new StringBuilder(str.replaceAll("[^a-zA-Z]", "")).reverse().toString();
+    }
+}
---
+++ Simple Fun 176 Reverse Letter/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Kata.reverseLetter("krishan"));
+        System.out.println(Kata.reverseLetter("ultr53o?n"));
+    }
+}
