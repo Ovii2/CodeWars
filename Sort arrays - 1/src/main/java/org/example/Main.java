c62ed30a912fae7231b32131ab3daa5b78d7d66c
Ovidijus <ovidijus.e@gmail.com>
2025-05-10 07:08:33 +0300

    Sort arrays - 1

---
+++ Sort arrays - 1/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(SortArray.sortArray(new int[]{2, 3, 1})));
+    }
+}
---
+++ Sort arrays - 1/src/main/java/org/example/SortArray.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class SortArray {
+    public static Object[] sortArray(Object[] names) {
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static long[] sortArray(long[] names) {
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static int[] sortArray(int[] names) {
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static double[] sortArray(double[] names) {
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static float[] sortArray(float[] names) {
+        Arrays.sort(names);
+        return names;
+    }
+}
