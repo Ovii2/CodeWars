b0c5e5ff5b43e79d85a7be2aea5bde6bfb2cd02e
Ovidijus <ovidijus.e@gmail.com>
2025-05-20 20:22:59 +0300

    Drying Potatoes

---
+++ Drying Potatoes/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Potatoes.potatoes(99,100,98));
+        System.out.println(Potatoes.potatoes(82,127,80));
+        System.out.println(Potatoes.potatoes(93,129,91));
+    }
+}
