03db43d92ede112f405a6040f677bc2a5c944b66
Ovidijus <ovidijus.e@gmail.com>
2025-05-24 17:31:21 +0300

    Sum Arrays

---
+++ Sum Arrays/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(SumArray.sum((new double[]{1, 5.2, 4, 0, -1})));
+    }
+}
---
+++ Sum Arrays/src/main/java/org/example/SumArray.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class SumArray {
+    public static double sum(double[] numbers) {
+        return numbers.length > 0 ? Arrays.stream(numbers)
+                .sum() : 0;
+    }
+}
