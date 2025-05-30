ab7b87f5d0daedb782addd8a2f3365cba769e530
Ovidijus <ovidijus.e@gmail.com>
2025-05-30 18:46:21 +0300

    How good are you really?

---
+++ How good are you really/src/main/java/org/example/Kata.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Kata {
+    public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+        return Arrays.stream(classPoints)
+                .average()
+                .getAsDouble() < yourPoints;
+    }
+}
---
+++ How good are you really/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Kata.betterThanAverage(new int[]{2, 3}, 5));
+    }
+}
+
