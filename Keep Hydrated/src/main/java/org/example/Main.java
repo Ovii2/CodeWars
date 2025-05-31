41bd4661b12b64158092d72458332e97722c24d3
Ovidijus <ovidijus.e@gmail.com>
2025-05-31 05:57:29 +0300

    KeepHydrated

---
+++ Keep Hydrated/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(KeepHydrated.liters(3));
+        System.out.println(KeepHydrated.liters(6.7));
+        System.out.println(KeepHydrated.liters(11.8));
+    }
+}
