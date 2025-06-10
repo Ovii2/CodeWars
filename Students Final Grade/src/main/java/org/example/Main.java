e0c68ec2206b28162be0608f8e1d7bea8440d565
Ovidijus <ovidijus.e@gmail.com>
2025-06-10 19:48:25 +0300

    Student's Final Grade

---
+++ Students Final Grade/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(StudentFinalGrade.finalGrade(100,12));
+        System.out.println(StudentFinalGrade.finalGrade(85,5));
+        System.out.println(StudentFinalGrade.finalGrade(55,3));
+    }
+}
