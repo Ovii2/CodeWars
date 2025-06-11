3657c98fc7df5dd1c0ec956b6a73f059d916fb71
Ovidijus <ovidijus.e@gmail.com>
2025-06-11 19:32:15 +0300

    Volume of a Cuboid

---
+++ Volume of a Cuboid/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Kata.getVolumeOfCuboid(1.2, 2, 2));
+        System.out.println(Kata.getVolumeOfCuboid(6.3, 2, 5));
+    }
+}
