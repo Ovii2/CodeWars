6ac147d1c175b71f14677a33cc58bac2aafb64f1
Ovidijus <ovidijus.e@gmail.com>
2025-06-23 18:34:28 +0300

    Convert a String to a Number!

---
+++ Convert a String to a Number/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(StringToNumber.stringToNumber("1234"));
+        System.out.println(StringToNumber.stringToNumber("-7"));
+    }
+}
