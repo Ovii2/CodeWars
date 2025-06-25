8a48643293d01ca775bad1f254d6a8686aef26b0
Ovidijus <ovidijus.e@gmail.com>
2025-06-25 18:31:56 +0300

    MakeUpperCase

---
+++ MakeUpperCase/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Upper.makeUpperCase("hello"));
+        System.out.println(Upper.makeUpperCase("bye"));
+    }
+}
---
+++ MakeUpperCase/src/main/java/org/example/Upper.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Upper {
+    public static String makeUpperCase(String str) {
+        return str.toUpperCase();
+    }
+}
