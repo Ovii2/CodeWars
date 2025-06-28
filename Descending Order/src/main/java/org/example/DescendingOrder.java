3254bb9af35fd05a96c3b1e8f0f748b6475383a3
Ovidijus <ovidijus.e@gmail.com>
2025-06-28 06:29:11 +0300

    Descending Order

---
+++ Descending Order/src/main/java/org/example/DescendingOrder.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
+
+public class DescendingOrder {
+    public static int sortDesc(final int num) {
+        return Integer.parseInt(
+                String.valueOf(num)
+                        .chars()
+                        .mapToObj(c -> (char) c)
+                        .sorted(Comparator.reverseOrder())
+                        .map(String::valueOf)
+                        .collect(Collectors.joining())
+        );
+    }
+}
---
+++ Descending Order/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DescendingOrder.sortDesc(0));
+        System.out.println(DescendingOrder.sortDesc(15));
+    }
+}
