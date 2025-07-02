63529078866b1b029fd31a7506c4f2ea428db028
Ovidijus <ovidijus.e@gmail.com>
2025-07-02 18:25:22 +0300

    Third Angle of a Triangle

---
+++ Third Angle of a Triangle/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(ThirdAngle.otherAngle(45, 12));
+        System.out.println(ThirdAngle.otherAngle(50, 60));
+    }
+}
