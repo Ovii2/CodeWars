04b2208aafd147495db62e30bfe521af95c41525
Ovidijus <ovidijus.e@gmail.com>
2025-07-09 07:08:25 +0300

    Grasshopper - Terminal game move function

---
+++ Grasshopper - Terminal game move function/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Move.move(0,4));
+        System.out.println(Move.move(3,6));
+        System.out.println(Move.move(2,5));
+    }
+}
