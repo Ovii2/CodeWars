68fbacb99d759537f35fcedea7eeaf7c7f2abcd3
Ovidijus <ovidijus.e@gmail.com>
2025-07-17 09:22:18 +0300

    Convert boolean values to strings 'Yes' or 'No'.

---
+++ Convert boolean values to strings Yes or No/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(YesOrNo.boolToWord(true));
+        System.out.println(YesOrNo.boolToWord(false));
+    }
+}
