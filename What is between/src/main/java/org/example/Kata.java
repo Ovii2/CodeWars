074814d7cb75ddf99da656bd050923e3f60f1f1d
Ovidijus <ovidijus.e@gmail.com>
2025-07-20 07:39:15 +0300

    What is between?

---
+++ What is between/src/main/java/org/example/Kata.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.stream.IntStream;
+
+public class Kata {
+    public static int[] between(int a, int b) {
+        return IntStream.rangeClosed(a, b)
+                .toArray();
+    }
+}
---
+++ What is between/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(Kata.between(1, 4)));
+        System.out.println(Arrays.toString(Kata.between(-2, 2)));
+
+    }
+}
