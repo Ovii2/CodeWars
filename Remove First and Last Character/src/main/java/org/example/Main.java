7430ba38c2906e71d628fcf685958a23eb9454fa
Ovidijus <ovidijus.e@gmail.com>
2025-07-27 06:23:07 +0300

    Remove First and Last Character

---
+++ Remove First and Last Character/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(RemoveChars.remove("person"));
+        System.out.println(RemoveChars.remove("ab"));
+        System.out.println(RemoveChars.remove("xyz"));
+    }
+}
