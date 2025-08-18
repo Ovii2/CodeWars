740edc12dfba9a396552d10d3e61a112e8ebd09b
Ovidijus <ovidijus.e@gmail.com>
2025-08-18 06:13:04 +0300

    Grasshopper - Summation

---
+++ Grasshopper - Summation/src/main/java/org/example/GrassHopper.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.stream.IntStream;
+
+public class GrassHopper {
+    public static int summation(int n) {
+        return IntStream.rangeClosed(1, n)
+                .sum();
+    }
+}
---
+++ Grasshopper - Summation/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(GrassHopper.summation(2));
+        System.out.println(GrassHopper.summation(8));
+    }
+}
