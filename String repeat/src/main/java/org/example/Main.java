d592f905c3827c2ecae5c5262319416e01f1cd14
Ovidijus <ovidijus.e@gmail.com>
2025-08-25 18:20:21 +0300

    String repeat

---
+++ String repeat/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Solution.repeatStr(4, "a"));
+        System.out.println(Solution.repeatStr(3, "Hello"));
+    }
+}
