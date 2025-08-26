1189da9e6478e8f78a39e011f68e72968781690d
Ovidijus <ovidijus.e@gmail.com>
2025-08-26 18:07:43 +0300

    DNA to RNA Conversion

---
+++ DNA to RNA Conversion/src/main/java/org/example/Bio.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Bio {
+    public static String dnaToRna(String dna) {
+        return dna.replace("T", "U");
+    }
+}
---
+++ DNA to RNA Conversion/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Bio.dnaToRna("TTTTT"));
+        System.out.println(Bio.dnaToRna("GCAT"));
+    }
+}
