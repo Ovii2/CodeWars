012a4b77dac5a71fe5dc26b3ac4534623974c590
Ovidijus <ovidijus.e@gmail.com>
2025-08-30 14:46:36 +0300

    Beginner - Reduce but Grow

---
+++ Beginner - Reduce but Grow/src/main/java/org/example/Kata.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Kata {
+    public static int grow(int[] x) {
+        return Arrays.stream(x)
+                .reduce(1, (a, b) -> a * b);
+    }
+}
---
+++ Beginner - Reduce but Grow/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Kata.grow(new int[]{1, 2, 3, 4}));
+        System.out.println(Kata.grow(new int[]{4, 1, 1, 1, 4}));
+
+    }
+}
