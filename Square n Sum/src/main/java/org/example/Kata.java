7bddeace54c9da6ae1c2457e6ea86a81295a4611
Ovidijus <ovidijus.e@gmail.com>
2025-08-31 06:56:44 +0300

    Square(n) Sum

---
+++ Square n Sum/src/main/java/org/example/Kata.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class Kata {
+    public static int squareSum(int[] n) {
+        return Arrays.stream(n)
+                .map(i -> i * i)
+                .sum();
+    }
+}
---
+++ Square n Sum/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Kata.squareSum(new int[]{1, 2, 2}));
+        System.out.println(Kata.squareSum(new int[]{5, -3, 4}));
+    }
+}
