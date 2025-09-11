d60c77b3f99cb6b7b3bb7762990349e616a04f17
Ovidijus <ovidijus.e@gmail.com>
2025-09-11 20:12:53 +0300

    Quarter of the year

---
+++ Quarter of the year/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Kata.quarterOf(3));
+        System.out.println(Kata.quarterOf(8));
+        System.out.println(Kata.quarterOf(11));
+    }
+}
