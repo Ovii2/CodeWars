45ed92a8cba954b8c96e644b8634c364dc17bc34
Ovidijus <ovidijus.e@gmail.com>
2025-10-04 07:49:09 +0300

    Parse nice int from char problem

---
+++ Parse nice int from char problem/src/main/java/org/example/CharProblem.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class CharProblem {
+    public static int howOld(final String herOld) {
+        return Integer.parseInt(herOld.split(" ")[0]);
+    }
+}
---
+++ Parse nice int from char problem/src/main/java/org/example/Main.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        IO.println(CharProblem.howOld("5 years old"));
+    }
+}
