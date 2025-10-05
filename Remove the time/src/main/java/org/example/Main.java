51957663547ea2c38ae0664732cbfaa2768aa2c5
Ovidijus <ovidijus.e@gmail.com>
2025-10-05 08:47:13 +0300

    Remove the time

---
+++ Remove the time/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        IO.println(Solution.shortenToDate("Friday May 2, 7pm"));
+        IO.println(Solution.shortenToDate("Tuesday January 29, 10pm"));
+    }
+}
---
+++ Remove the time/src/main/java/org/example/Solution.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Solution {
+    public static String shortenToDate(String longDate) {
+        return longDate.split(",")[0];
+    }
+}
