0c358b60564676f4a1f62e35569c2d1302d79952
Ovidijus <ovidijus.e@gmail.com>
2025-10-09 18:48:36 +0300

    Find the position!

---
+++ Find the position/src/main/java/org/example/Kata.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Kata {
+    public static String position(char alphabet) {
+        int position = alphabet - 'a' + 1;
+        return "Position of alphabet: %d".formatted(position);
+    }
+}
---
+++ Find the position/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        IO.println(Kata.position('a'));
+        IO.println(Kata.position('z'));
+        IO.println(Kata.position('e'));
+    }
+}
