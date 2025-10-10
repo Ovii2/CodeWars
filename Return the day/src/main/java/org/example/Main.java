4ed8a867fa82ace2f8a0ca92bb8b7743e4684cdd
Ovidijus <ovidijus.e@gmail.com>
2025-10-10 17:59:19 +0300

    Return the day

---
+++ Return the day/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        IO.println(DayOfWeek.getDay(1));
+        IO.println(DayOfWeek.getDay(2));
+        IO.println(DayOfWeek.getDay(3));
+        IO.println(DayOfWeek.getDay(4));
+        IO.println(DayOfWeek.getDay(0));
+    }
+}
