3cb5c1bcebf3ce55a4a8b32fcccb80d7ee446976
Ovidijus <ovidijus.e@gmail.com>
2025-10-24 17:07:04 +0300

    Return Negative

---
+++ Return Negative/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        IO.println(Kata.makeNegative(1));
+        IO.println(Kata.makeNegative(-5));
+        IO.println(Kata.makeNegative(42));
+        IO.println(Kata.makeNegative(0));
+    }
+}
