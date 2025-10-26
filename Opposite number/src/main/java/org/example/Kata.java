5fbccd1fe8a9521b98194feadcb9ed59934cd771
Ovidijus <ovidijus.e@gmail.com>
2025-10-26 16:29:55 +0200

    Opposite number

---
+++ Opposite number/src/main/java/org/example/Kata.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Kata {
+    public static int opposite(int number) {
+        return number < 0 ? Math.abs(number) : -number;
+    }
+}
---
+++ Opposite number/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    static void main() {
+        IO.println(Kata.opposite(1));
+        IO.println(Kata.opposite(14));
+        IO.println(Kata.opposite(-34));
+    }
+}
